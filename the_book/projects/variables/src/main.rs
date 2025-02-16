2756f6f993c23bd306ca5931042d3f9251227ac9
jdelles <james.delles@gmail.com>
2025-02-15 22:25:53 -0500

    Done with Chapter 3 in Let's Get Rusty videos

--- the_book/projects/variables/src/main.rs
+++ the_book/projects/variables/src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
-    let x = 5;
+    let mut x = 5;
+    println!("The values of x is: {}", x);
+    x = 6;
+    println!("The values of x is: {}", x);
 
+    const SUBSCRIBER_COUNT: u32 = 100_000;
+    
     let x = x + 1;
 
     {
@@ -9,4 +14,51 @@ fn main() {
     }
 
     println!("The values of x is {x}");
+
+    // scalar types
+    // Integer
+
+    let a: i32 = 98_222; // Decimal
+    let b: i32 = 0xff; // Hex
+    let c: i32 = 0o77; // Octal
+    let d: i32 = 0b1111_0000; // Binary
+    let e: u8 = b'A'; // Byte (u8 only)
+    
+    
+    // Floating-point numbers
+
+    let f: f32 = 2.0; // f32
+    let g: f64 = 3.0; // f64
+
+    // Numeric operations
+
+    let sum = 5 + 10;
+    let difference = 95.5 - 4.3;
+    let product = 4 * 30;
+    let quotient = 56.7 / 32.2;
+    let remainder = 43 % 5;
+
+    // Booleans
+
+    let t: bool = true;
+    let f: bool = false;
+
+    // Characters
+
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';
+
+    // Compound types
+
+    // Tuple
+
+    let tup: (&str, i32) = ("Let's Get Rusty!", 100_000);
+    let (channel, sub_count) = tup;
+    let sub_count = tup.1;
+
+    let error_codes = [200, 404, 500];
+    let not_found: i32 = error_codes[2];
+
+    let byte = [0; 8]; 
 }
