fdd890c6c78cc7b1d0f525f805b0d344d54382ff
jdelles <james.delles@gmail.com>
2025-02-19 22:13:32 -0500

    Chapter 6 - from Let's Get Rusty! videos.

---
+++ the_book/projects/enumurations/src/main.rs
@@ -0,0 +1,32 @@
+enum IpAddrKind {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32), 
+}
+
+impl Message {
+    fn some_function() {
+        println!("Some function");
+    }
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn main() {
+    let localhost = IpAddrKind {
+        kind: IpAddrKind::V4(127, 0, 0, 1),
+    };
+}
+
+fun route(ip_kind: IpAddrKind) {
+
+}
